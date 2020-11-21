978fe8c6f222759764b8b311117faaf70d37c2e4
Sierra Chapman <siechap@gmail.com>
2020-11-20 17:39:26 -0800

    make cache into a hash map/linked list hybrid

--- src/cache.ts
+++ src/cache.ts
@@ -1,12 +1,34 @@
+interface CacheItem {
+    name: string;
+    value: any;
+    next: CacheItem | Tail;
+    prev: CacheItem | Head;
+}
+
+interface Head {
+    next?: CacheItem | Tail;
+}
+
+interface Tail {
+    prev?: CacheItem | Head;
+}
+
 class Cache {
-    private data: { [name: string]: any };
+    private data: { [name: string]: CacheItem };
+    private head: Head;
+    private tail: Tail;
 
     constructor() {
         this.data = {};
+        this.head = {};
+        this.tail = {};
+
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
     }
 
     add(name: string, value: any) {
-        this.data = { ...this.data, [name]: value };
+        this.data = { ...this.data, [name]: { name, value, next: this.tail, prev: this.head } };
     }
 
     remove(name: string): boolean {
